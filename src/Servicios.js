4f53e0a1ea79ff72b4d1b001610b5f3281a64914
lunaserrano <48137753+lunaserrano@users.noreply.github.com>
2023-09-06 20:57:52 -0600

    commit2

--- src/Servicios.js
+++ src/Servicios.js
@@ -3,12 +3,23 @@ import React from 'react';
 function Servicios() {
   return (
 
-<div className='container'>
-
-    <div>
-      <h1>Página de Servicios</h1>
-      <p>Bienvenido a nuestra página de Servicios.</p>
-    </div>
+    <div className='container'>
+        <div class="row ">
+            <div className='card'>
+                <div className='card-title'>Registro de Servicios</div>
+                <div className='card-body'>
+                    <div className='container-fluid'>
+                        <div className='row'>
+                            <div className='form-group'>
+                                <label>Servicio</label>
+                                <input type='text' name='txt_servicio' className='form-control'/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='card-footer'>Lista de botones</div>
+            </div>
+        </div>
 
     </div>
 
